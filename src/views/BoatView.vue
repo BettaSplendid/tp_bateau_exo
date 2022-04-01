455b0f8a4099b989ff9460efbf517ab674af863b
forbidden Betta <forbiddenBetta@gmail.com>
2022-04-01 14:49:37 +0200

    First commit

---
+++ src/views/BoatView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Here be the form, yarr</h1>
+    <div>
+      <form action="">
+          <input type="text" name="nom_pro" id="nom_pro">
+          <input type="text" name="nom_bat" id="nom_bat">
+          <input type="text" name="taille" id="taille">
+          <input type="number" name="prix" id="prix">
+      </form>
+    </div>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+
+</script>
+
